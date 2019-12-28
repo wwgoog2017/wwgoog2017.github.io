00f7636193c0d615e73d08f93fdc9cf84f8a5067
wwgoog2017 <57285667+wwgoog2017@users.noreply.github.com>
2019-12-28 16:45:43 +0800

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,10 +1,10 @@
 
 $("#logo").animate({
-  height:"+=210px",
-  width: "+=210px"
+  height:"+=180px",
+  width: "+=180px"
   },1500);        // increase in 1.5 second
 
 $("#logo").animate({
-  height: "-=200px",
-  width: "-=200px"
+  height: "-=150px",
+  width: "-=150px"
 },1500);
